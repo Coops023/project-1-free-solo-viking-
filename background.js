7a40250ed426c03dd74f8fd00093ae17e6a9aa8b
Coops023 <bjorkc27@gmail.com>
2021-08-22 12:20:16 +0200

    Project free solo viking, WIP

---
+++ background.js
@@ -0,0 +1,16 @@
+const bgImg = document.createElement('img');
+
+bgImg.src = '/assets/background.png';
+
+class Background {
+    constructor(canvasContext) {
+        this.ctx = canvasContext;
+        this.x = 0;
+        this.y = 0;
+        this.width = 500;
+        this.heigh = 500;
+    }
+    draw() {
+        this.ctx.drawImage(bgImg, this.x, this.y, this.width, this.height);
+    }
+}
